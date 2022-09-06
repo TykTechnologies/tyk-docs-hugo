debbbc8116bc8be5f7467c50b33c0875e7c7f9fc
Zaid Albirawi <zaid@tyk.io>
2022-09-06 17:42:07 -0400

    Add analyzer app

---
+++ tyk-docs/static/apps/analyzer/precache-manifest.5226449845f22d856b71645d7940dd10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf7afa891c70eec1bc8",
+    "url": "/static/css/main.286ece6d.chunk.css"
+  },
+  {
+    "revision": "dcf7afa891c70eec1bc8",
+    "url": "/static/js/main.dcf7afa8.chunk.js"
+  },
+  {
+    "revision": "f25528436ada661202f5",
+    "url": "/static/js/1.f2552843.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "223e318531b00c51b25bc5fda0b5e959",
+    "url": "/static/media/Smoolthan Bold.223e3185.otf"
+  },
+  {
+    "revision": "8257d4bc2732d6e9351230af2f7fddf1",
+    "url": "/index.html"
+  }
+];
