a966332f98e8546b529886f94937a62a57f6b77a
Leonid Bugaev <leonsbox@gmail.com>
2022-12-10 23:35:00 +0300

    Added new CI job to check for broken Hugo links
    New job will compare URL generated by Hugo (including all aliases), before and
    after latest PR change, and if URLs are missing will fail, with list of missing
    URLs.

---
+++ scripts/urlcheck.sh
@@ -0,0 +1,16 @@
+CURRENT_COMMIT=$(git rev-parse HEAD)
+hugo
+cp ./public/urlcheck.json /tmp/urlcheck.new.json
+git checkout $1 || true
+hugo
+cp ./public/urlcheck.json /tmp/urlcheck.prev.json
+git checkout $CURRENT_COMMIT
+NEW_URLS=$(cat /tmp/urlcheck.new.json | jq -r '.path')
+PREV_URLS=$(cat /tmp/urlcheck.pref.json | jq -r '.path')
+BROKEN_URLS=$(comm -3 -1 <(echo $NEW_URLS | sort) <(echo $PREV_URLS | sort))
+
+if [ -n "$BROKEN_URL" ]; then
+  echo "The following links are broken"
+  echo $BROKEN_URLS
+  exit 1
+fi
