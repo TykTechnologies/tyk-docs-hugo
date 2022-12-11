96e2305ae094dc8a6d2dfb28d9d2ae1b626733e5
Leonid Bugaev <leonsbox@gmail.com>
2022-12-11 12:30:51 +0300

    FIx script

--- scripts/urlcheck.sh
+++ scripts/urlcheck.sh
@@ -3,10 +3,10 @@ set -e
 CURRENT_COMMIT=$(git rev-parse HEAD)
 hugo
 cp ./public/urlcheck.json /tmp/urlcheck.new.json
-git checkout $1 || true
+git checkout $1
 hugo
 cp ./public/urlcheck.json /tmp/urlcheck.prev.json
-git checkout $CURRENT_COMMIT
+git checkout - 
 NEW_URLS=$(cat /tmp/urlcheck.new.json | jq -r '.path')
 PREV_URLS=$(cat /tmp/urlcheck.prev.json | jq -r '.path')
 BROKEN_URLS=$(comm -3 -1 <(echo $NEW_URLS | sort) <(echo $PREV_URLS | sort))
