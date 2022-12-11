fe22c754c1dab9db98197d39697a7d0eeb966200
Leonid Bugaev <leonsbox@gmail.com>
2022-12-11 12:49:20 +0300

    Fix script

--- scripts/urlcheck.sh
+++ scripts/urlcheck.sh
@@ -13,7 +13,7 @@ NEW_URLS=$(cat /tmp/urlcheck.new.json | jq -r '.path')
 PREV_URLS=$(cat /tmp/urlcheck.prev.json | jq -r '.path')
 BROKEN_URLS=$(comm -3 -1 <(echo $NEW_URLS | sort) <(echo $PREV_URLS | sort))
 
-if [ -n "$BROKEN_URL" ]; then
+if [ -n "$BROKEN_URLS" ]; then
   echo "The following links are broken"
   echo $BROKEN_URLS
   exit 1
