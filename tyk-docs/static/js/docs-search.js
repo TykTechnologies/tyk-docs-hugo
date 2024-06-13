5425497a73c26f9001c9a29016e9084daa84b5e1
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-06-13 07:07:55 +0300

    change emv in v3

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -5,18 +5,14 @@
    * INIT INSTANT SEARCH
    */
   docsearch({
-    algoliaOptions: {
-    hitsPerPage: 200,
-    // See https://www.algolia.com/doc/api-reference/api-parameters/
-    },
     // Your apiKey and indexName will be given to you once
     // we create your config
-    apiKey: 'ALGOLIA_API_KEY',
-    indexName: 'docsearch',
-    appId: 'ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
+    apiKey: 'ALGOLIA_API_KEY_v3',
+    indexName: "tyk",
+    appId: 'ALGOLIA_APP_ID_v3', // Should be only included if you are running DocSearch on your own.
     // Replace inputSelector with a CSS selector
     // matching your search input
-    container: inputSelector,
+    container: "#search_container",
     // Set debug to true to inspect the dropdown
     // debug: true,
   });
