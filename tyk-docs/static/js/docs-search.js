b3d98a3def6589e220db7cbf49eb654463e1ac83
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2024-06-13 07:17:17 +0300

    change emv in v3

--- tyk-docs/static/js/docs-search.js
+++ tyk-docs/static/js/docs-search.js
@@ -7,9 +7,9 @@
   docsearch({
     // Your apiKey and indexName will be given to you once
     // we create your config
-    apiKey: 'ALGOLIA_API_KEY_v3',
+    apiKey: 'NEW_ALGOLIA_API_KEY',
     indexName: "tyk",
-    appId: 'ALGOLIA_APP_ID_v3', // Should be only included if you are running DocSearch on your own.
+    appId: 'NEW_ALGOLIA_APP_ID', // Should be only included if you are running DocSearch on your own.
     // Replace inputSelector with a CSS selector
     // matching your search input
     container: "#search_container",
