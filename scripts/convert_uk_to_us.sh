e08fab724203e46167e65b6c8bebf4b4887cc84b
Yaara <yaara@tyk.io>
2024-06-21 17:24:21 +0100

    Update scripts/convert_uk_to_us.sh
    Co-authored-by: dcs3spp <dcs3spp@users.noreply.github.com>

--- scripts/convert_uk_to_us.sh
+++ scripts/convert_uk_to_us.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+#===============================================================================
+#
+#       FILE: convert_uk_to_us.sh
+# 
+#       USAGE: ./run-menu-gen.sh <csv_file> <directory>
+# 
+#       DESCRIPTION: This script reads a CSV file containing pairs of UK and US
+#                words, then recursively searches through the specified directory
+#                to replace all instances of the UK words with their US counterparts
+#                in the files found.
+# 
+#       OPTIONS: 
+#.               <csv_file> - Path to the CSV file containing UK and US word pairs.
+#                <directory> - Path to the directory where replacements should be made.
+#
+#       REQUIREMENTS: The CSV file should be formatted with each line containing 
+#                two words separated by a comma, where the first word is the UK 
+#                word and the second is the US word. The script also requires 
+#                write permissions for the files in the target directory.
+#  
+#       NOTES: The script uses `find` to search for files and `sed` to perform 
+#                the replacements. It handles basic error checking and ensures 
+#                that the specified CSV file and directory exist before proceeding.
+#===============================================================================
 set -e  # Exit immediately if a command exits with a non-zero status.
 
 # Function to check the last command's exit status
