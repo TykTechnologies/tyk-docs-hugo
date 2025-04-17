4e40d0912f6354efc68514aecba8e67b650dd257
Yaara <letz.yaara@gmail.com>
2025-04-17 11:46:14 +0100

    simpler implementation

--- tyk-docs/static/js/clickable-directories.js
+++ tyk-docs/static/js/clickable-directories.js
@@ -1,41 +1,47 @@
 /**
  * Enable directory items in navigation to be both clickable links and expandable containers.
+ * Uses traditional navigation to avoid flickering.
  */
 (function($) {
-  function init() {
+  function initDirectoryLinks() {
+    // First make sure parents of active items are expanded
+    $('.active').parents('li').removeClass('st-collapsed').addClass('st-open');
+    
+    // Only handle the mousedown event to prevent SimpleTree's handling
+    // Let the browser handle the normal click behavior for navigation
     $('.category-Directory > a[href]').each(function() {
       var $link = $(this);
-      var $li = $link.parent();
-      var href = $link.attr('href');
       
-      $link.off();
+      // Clear any previous handlers
+      $link.off('mousedown');
       
-      $link.on('click', function(e) {
-        window.location = href;
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-      });
+      // Disable turbolinks for these links to avoid flickering
+      $link.attr('data-turbolinks', 'false');
       
+      // Only handle mousedown to prevent SimpleTree from intercepting
       $link.on('mousedown', function(e) {
+        // Stop propagation to prevent SimpleTree handlers
         e.stopPropagation();
+        e.stopImmediatePropagation();
+        
+        // Let the normal click/navigation behavior happen
         return true;
       });
     });
-    
-    function ensureParentsOpen() {
-      $('.active').parents('li').removeClass('st-collapsed').addClass('st-open');
-    }
-    
-    ensureParentsOpen();
-    setInterval(ensureParentsOpen, 300);
   }
-  
+
+  // Run on document ready
   $(document).ready(function() {
-    setTimeout(init, 500);
+    setTimeout(initDirectoryLinks, 50);
   });
   
+  // Also run after Turbolinks navigation
   $(document).on('turbolinks:load', function() {
-    setTimeout(init, 500);
+    setTimeout(initDirectoryLinks, 50);
   });
+  
+  // Run periodically to ensure parent directories stay expanded
+  setInterval(function() {
+    $('.active').parents('li').removeClass('st-collapsed').addClass('st-open');
+  }, 500);
 })(jQuery);
