61a69e703f10671a88ae559d028848abc78e1adc
itachi sasuke <8012032+Keithwachira@users.noreply.github.com>
2025-04-17 14:59:41 +0300

    fix flicker in site

--- tyk-docs/static/js/scroll_to_active_tab.js
+++ tyk-docs/static/js/scroll_to_active_tab.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const activeItem = document.querySelector('.st-treed li.active');
     if (activeItem) {
         activeItem.scrollIntoView({
-            behavior: 'instant',
+            behavior: 'smooth',
             block: 'center',
         });
     }
